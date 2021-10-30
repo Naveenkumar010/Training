3b6b55c5c4f8c808c02962750a67310252f2dbb5
Naveenkumar010 <93249394+Naveenkumar010@users.noreply.github.com>
2021-10-30 11:30:02 +0530

    Add files via upload

---
+++ TANKLEVE.C
@@ -0,0 +1,28 @@
+#include<stdio.h>
+#include<conio.h>
+int a,b,i;
+int main()
+{
+clrscr();
+printf("Enter the current tank level:");
+scanf("%d",&a);
+printf("Enter the max water level:");
+scanf("%d",&b);
+for(i=a;i<=b;i++)
+{
+printf("tank is %d level \n",i);
+if(i==b)
+{
+printf("Reached Max level");
+}
+}
+for(i=a;i>=b;i--)
+{
+printf("tank level is %d \n",i);
+if(i==0)
+{
+printf("tank level is empty");
+}
+}
+return 0;
+}
